3d32704ef8aba14122f455992eae50fb9d2300d9
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-03-07 23:32:34 +0400

    b

---
+++ script.js
@@ -0,0 +1,27 @@
+// const questions = Array.from(document.querySelectorAll("span"));
+// console.log(questions);
+// const answers = Array.from(document.querySelectorAll("p"));
+// console.log(answers);
+// const divsArr = Array.from(document.querySelectorAll(".d"));
+// console.log(divsArr[0].children[1].textContent);
+// const arrows = Array.from(document.querySelectorAll(".arrow-down "));
+// console.log(arrows);
+// console.log(divsArr[0].children[0].children[1]);
+// for (let i = 0; i < divsArr.length; i++) {
+//   divsArr[i].addEventListener("click", () => {
+//     for (let j = 0; j < divsArr.length; j++) {
+//       divsArr[j].children[1].style.display = "none";
+//       // divsArr[j].children[0].children[1].style.tra
+//     }
+//     divsArr[i].children[1].style.display = "block";
+//   });
+// }
+
+const questionsDivs = document.querySelector(".texts-div");
+
+questionsDivs.addEventListener("click", (event) => {
+  const element = event.target.closest(".d");
+  if (!element) return;
+  console.log(element.classlist);
+  element.classlist.toggle("active");
+});
